07e02f1d6972469b42ea5a7b3a62a93a65de8077
VIVEK YADAV <vivyadav34@gmail.com>
2026-09-08 20:19:13 +0530

    Time: 7 ms (98.23%), Space: 12.3 MB (58.53%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+
+        result = []
+        i = 0
+        j = 0
+
+        while i < len(word1) or j < len(word2):
+
+            if i < len(word1):
+                result.append(word1[i])
+                i += 1
+
+            if j < len(word2):
+                result.append(word2[j])
+                j += 1
+
+        return ''.join(result)
